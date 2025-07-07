2a7c53684b6c3288083be6d8b7e8c2dcfb254fd8
Dafni Anagnostopoulou <89984193+dafnianagno@users.noreply.github.com>
2025-07-07 16:09:27 +0300

    Add files via upload

---
+++ HM/setup/1_create_assets.sql
@@ -0,0 +1,56 @@
+--
+-- NOTE: everything is DROPPED and re-created
+--
+--
+-- NOTE: this file works but is work in progress. For now, please don't change the
+-- initial parameters (db_name, schema_name, etc.)
+--
+
+USE ROLE ACCOUNTADMIN;
+
+-- set up of constants, change the names in this section of what assets you'd like to create,
+-- no need to touch the rest of the code
+
+SET db_name = 'hm_db';
+SET schema_data_name = 'hm_data';
+SET schema_data_full_name = $db_name||'.'||$schema_data_name; -- fully-qualified
+SET schema_purchase_name = 'hm_purchase';
+SET schema_purchase_full_name = $db_name||'.'||$schema_purchase_name; -- fully-qualified
+SET schema_churn_name = 'hm_churn';
+SET schema_churn_full_name = $db_name||'.'||$schema_churn_name; -- fully-qualified
+SET stage_name = 'hm_stage'; -- fully-qualified
+SET stage_full_name = $schema_full_name||'.'||$stage_name;
+SET wh_name = 'hm_wh';
+SET wh_size = 'X-SMALL';
+SET role_name = 'SYSADMIN';   -- what role will have access to the db/warehouse/schema etc.
+
+--
+-- assets
+--
+-- cleanup
+DROP DATABASE IF EXISTS identifier($db_name);
+DROP WAREHOUSE IF EXISTS identifier($wh_name);
+
+-- create role
+CREATE ROLE IF NOT EXISTS identifier($role_name);
+
+-- create a database
+CREATE DATABASE IF NOT EXISTS identifier($db_name);
+GRANT OWNERSHIP ON DATABASE identifier($db_name) TO ROLE identifier($role_name) COPY CURRENT GRANTS;
+USE DATABASE identifier($db_name);
+
+-- create warehouse
+CREATE OR REPLACE WAREHOUSE identifier($wh_name) WITH WAREHOUSE_SIZE = $wh_size;
+GRANT USAGE ON WAREHOUSE identifier($wh_name) TO ROLE identifier($role_name);
+
+-- create a schema
+CREATE SCHEMA IF NOT EXISTS identifier($schema_full_name);
+GRANT USAGE ON SCHEMA identifier($schema_full_name) TO ROLE identifier($role_name);
+USE SCHEMA identifier($schema_full_name);
+
+-- create a stage
+CREATE STAGE IF NOT EXISTS identifier($stage_full_name) DIRECTORY = ( ENABLE = true );
+GRANT READ ON STAGE identifier($stage_full_name) TO ROLE identifier($role_name);
+
+-- privilege for notebook
+GRANT CREATE NOTEBOOK ON SCHEMA identifier($schema_full_name) TO ROLE identifier($role_name);
